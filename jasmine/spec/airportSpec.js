08af8b92fc9cf9dc6ff43bb88be4bae6df18e547
ainsleybc <ab.chang@me.com>
2017-10-24 18:33:47 +0100

    added plane storage feature

--- jasmine/spec/airportSpec.js
+++ jasmine/spec/airportSpec.js
@@ -12,12 +12,24 @@ describe('airport', function () {
     it('has a default capacity of 10', function () {
       expect(airport.capacity()).toEqual(10);
     });
+    it('allows to override the default capacity', function () {
+      airport = new Airport(20)
+      expect(airport.capacity()).toEqual(20);      
+    });
 
   })
-  
-  it('instructs a plane to land at an airport', function () {
-    airport.land(plane);
-    expect(plane.land).toHaveBeenCalled();
+
+  describe('land', function () {
+    
+    it('instructs a plane to land at an airport', function () {
+      airport.land(plane);
+      expect(plane.land).toHaveBeenCalled();
+    });
+    it('stores a landed plane in the hangar', function () {
+      airport.land(plane);
+      expect(airport.planes()).toEqual(jasmine.arrayContaining([plane]));
+    });
+
   });
 
 });
--- jasmine/src/airport.js
+++ jasmine/src/airport.js
@@ -1,11 +1,17 @@
 function Airport(capacity = 10) {
   this._capacity = capacity
+  this._planes = []
 };
 
 Airport.prototype.land = function (plane) {
   plane.land();
+  this._planes.push(plane)
 };
 
 Airport.prototype.capacity = function (capacity) {
   return this._capacity;
+};
+
+Airport.prototype.planes = function () {
+  return this._planes;
 };
